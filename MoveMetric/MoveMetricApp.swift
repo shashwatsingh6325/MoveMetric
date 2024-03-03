b8e672addd290e4f222a236e073bc94a7d087c80
SHASHWAT SINGH <ss6325@srmist.edu.in>
2024-03-03 19:06:08 +0530

    Initial Commit

---
+++ MoveMetric/MoveMetricApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoveMetricApp.swift
+//  MoveMetric
+//
+//  Created by user2 on 03/03/24.
+//
+
+import SwiftUI
+
+@main
+struct MoveMetricApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
