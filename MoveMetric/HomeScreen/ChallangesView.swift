d48189c67ae4f8e9b6a59422983b91646b620cdb
SHASHWAT SINGH <ss6325@srmist.edu.in>
2024-04-04 13:15:03 +0530

    itinial commit 4-4-24

---
+++ MoveMetric/HomeScreen/ChallangesView.swift
@@ -0,0 +1,18 @@
+//
+//  ChallangesView.swift
+//  MoveMetric
+//
+//  Created by shashwat singh   on 26/03/24.
+//
+
+import SwiftUI
+
+struct ChallangesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChallangesView()
+}
